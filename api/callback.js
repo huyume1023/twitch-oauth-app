25665036be982255dfc8584d10077eccdaea670b
huyume1023 <huyume1023@gmail.com>
2025-03-28 22:22:25 +0800

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,13 @@
+// api/callback.js
+export default async function handler(req, res) {
+  const { code } = req.query;
+  if (!code) {
+    return res.status(400).send("Missing code");
+  }
+
+  // 這裡您可以處理 OAuth 交換 Token 的邏輯
+  console.log("Twitch 回傳的授權碼:", code);
+
+  res.send("已收到 Twitch OAuth 回傳的 code，您可以在控制台查看");
+}
 
